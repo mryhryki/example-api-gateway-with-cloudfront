433b2d8a9099c20d0cc276a4e191a6272a43d042
mryhryki <mryhryki@gmail.com>
2021-04-29 13:48:06 +0900

    Change log format.

--- measure/index.ts
+++ measure/index.ts
@@ -7,7 +7,7 @@ const getAverage = (results: number[]): number =>
 const measure = async (url: string, count: number): Promise<number[]> => {
   // Lambda のコールドスタートを考慮して、初回は計測対象にしない
   await fetch(url)
-  console.log(`[Measure: ${url}]`)
+  console.log("URL[%s]", url)
   const results: number[] = []
   for await (const c of Array.from({length: count}).map((_, i) => i + 1)) {
     const start = getNowUnixTimeMs()
@@ -15,13 +15,16 @@ const measure = async (url: string, count: number): Promise<number[]> => {
     const timeMs = getNowUnixTimeMs() - start
     const {random} = await res.json()
     results.push(timeMs)
-    console.debug("Round[%d] Res:%d (%s ms) [%s]", c, res.status, timeMs, random)
+    console.log("%s Res: %d (%s ms) [Random: %s]", `00${c}`.substr(-3, 3), res.status, timeMs, random)
   }
+  console.log("")
   return results;
 }
 
+console.log("")
 const {api_gateway_url, cloudfront_url} = JSON.parse(await Deno.readTextFile('./terraform.tfstate')).outputs
 const apiGatewayResults = await measure(api_gateway_url.value, COUNT)
 const cloudFrontResults = await measure(cloudfront_url.value, COUNT)
 console.log("API Gateway average response time:", getAverage(apiGatewayResults), "ms")
 console.log("CloudFront average response time:", getAverage(cloudFrontResults), "ms")
+console.log("")
